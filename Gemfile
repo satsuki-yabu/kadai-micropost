91047adeebe560c03301083cc076ed6dc77d82b2
satsuki.yabu <satsuki.yabu@techacademy.jp>
2020-08-03 08:13:16 +0000

    kadai-micropost

--- Gemfile
+++ Gemfile
@@ -58,3 +58,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'kaminari'
+
+gem 'pry-rails'
+gem 'pry-byebug', group: :development
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,11 @@ class UsersController < ApplicationController
     counts(@user)
   end
 
+  def likes
+    @user = User.find(params[:id])
+    @microposts = @user.fav_microposts.order(id: :desc).page(params[:page])
+  end
+
   private
 
   def user_params
